e2310dc17316138c8871ea666e776f8771712d28
thecederick <thecederick@gmail.com>
2019-05-12 12:14:31 +0300

    example

---
+++ src/main/java/com/example/asciidoctorexample/AsciidoctorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.asciidoctorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciidoctorExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsciidoctorExampleApplication.class, args);
+    }
+
+}
